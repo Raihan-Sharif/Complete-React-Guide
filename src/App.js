6ca75c3b95ea558e565e342b89a8c802752f0449
Raihan-Sharif <raihan.raju@gmail.com>
2020-09-15 23:05:36 +0600

    Add-Person component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
-class App extends Component{
-render(){
-  return(
-    <div className="App">
-    
-    </div>
-  )
-}
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hi am React App</h1>
+        <p>Oww this is really working!!</p>
+        <Person/>
+      </div>
+    )
+  }
 }
 
 export default App;
